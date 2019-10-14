59ae7ded20196bc35b1269c846dcd266e5c4c515
Joakim Ramer <jramer@me.com>
2019-10-14 21:52:38 +0200

    Version 1.0.0

--- package.js
+++ package.js
@@ -1,22 +1,27 @@
 Package.describe({
   name: 'bigowl:accounts-apple',
-  version: '0.0.1',
+  version: '1.0.0',
   summary: 'OAuth2 for Sign in with Apple',
   git: 'https://github.com/jramer/accounts-apple',
   documentation: 'README.md',
 });
 
 Package.onUse(api => {
+  api.versionsFrom('1.8.1');
   api.use(['ecmascript']);
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('bigowl:apple-oauth');
+  api.use('bigowl:apple-oauth@1.0.0');
   api.imply('bigowl:apple-oauth');
 
-  api.use(['accounts-ui', 'bigowl:apple-config-ui'], ['client', 'server'], {
-    weak: true,
-  });
+  api.use(
+    ['accounts-ui', 'bigowl:apple-config-ui@0.0.1'],
+    ['client', 'server'],
+    {
+      weak: true,
+    }
+  );
   api.addFiles('notice.js');
 
   api.addFiles('apple.js');
