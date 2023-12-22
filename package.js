db50ed5f50d822abff3118cd8154ceb80f7d881b
Renan Camargo de Castro <renan@blastradius.ai>
2023-12-22 09:19:15 -0300

    add meteor v3 support and update docs

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'quave:accounts-apple',
-  version: '3.0.0',
+  version: '4.0.0',
   summary: 'OAuth2 for Sign in with Apple - fork from bigowl',
   git: 'https://github.com/quavedev/accounts-apple',
   documentation: 'README.md',
 });
 
 Package.onUse(api => {
-  api.versionsFrom(['1.8.1', '2.3']);
+  api.versionsFrom(['1.8.1', '2.3', '3.0-alpha.15']);
   api.use(['ecmascript']);
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
